58686bfd5c35b5a0d37a66cac358fe6442881c9a
sid12261996 <sidharthrkc@gmail.com>
2024-03-11 09:53:19 +0530

    some refactoring and added gitignore and readme fils

--- src/Queries.java
+++ src/Queries.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 
-class Main {
+class Queries {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
